f514b46918ea6366d38a9f013e1c41da19ddb82e
Chanan Braunstein <chanan@bkbunch.com>
2023-05-11 09:40:15 -0600

    removed unneeded packages

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,59 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MyHomePage extends HookConsumerWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final tabController = useTabController(initialLength: 3);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ListView Text Issue'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.abc),
-            label: 'ABC',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.access_alarm),
-            label: 'Alarm',
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children: [
-            const Text('Click on the tabs and see if the text goes crazy'),
-            TabBar(
-              controller: tabController,
-              tabs: const [
-                Tab(
-                  text: 'Tab 1',
-                ),
-                Tab(
-                  text: 'Tab 2',
-                ),
-                Tab(
-                  text: 'Tab 3',
-                ),
-              ],
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView Text Issue'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.abc),
+              label: 'ABC',
             ),
-            Expanded(
-              child: TabBarView(
-                controller: tabController,
-                children: const [
-                  Text('Tab 1'),
-                  Text('Tab 2'),
-                  Text('Tab 3'),
-                ],
-              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_alarm),
+              label: 'Alarm',
             ),
           ],
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Text('Click on the tabs and see if the text goes crazy'),
+              TabBar(
+                tabs: [
+                  Tab(
+                    text: 'Tab 1',
+                  ),
+                  Tab(
+                    text: 'Tab 2',
+                  ),
+                  Tab(
+                    text: 'Tab 3',
+                  ),
+                ],
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Text('Tab 1'),
+                    Text('Tab 2'),
+                    Text('Tab 3'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
